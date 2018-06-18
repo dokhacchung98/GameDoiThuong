7609cc729e26e5a9f20b6b3e9785c51356494562
Đỗ Khắc Chung <ochung3@gmail.com>
2018-06-18 15:16:07 +0700

    first commit

---
+++ app/src/main/java/com/ae4/doi_thuong/ContactModel.java
@@ -0,0 +1,41 @@
+package com.ae4.doi_thuong;
+
+/**
+ * Created by Administrator on 19/01/2017.
+ */
+
+public class ContactModel {
+    private int giaDatCuocChinh;
+    private int imageCon;
+    private String tenCon;
+
+    public ContactModel(int giaDatCuocChinh, int imageCon, String tenCon) {
+        this.giaDatCuocChinh = giaDatCuocChinh;
+        this.imageCon = imageCon;
+        this.tenCon = tenCon;
+    }
+
+    public int getGiaDatCuocChinh() {
+        return giaDatCuocChinh;
+    }
+
+    public void setGiaDatCuocChinh(int giaDatCuocChinh) {
+        this.giaDatCuocChinh = giaDatCuocChinh;
+    }
+
+    public int getImageCon() {
+        return imageCon;
+    }
+
+    public void setImageCon(int imageCon) {
+        this.imageCon = imageCon;
+    }
+
+    public String getTenCon() {
+        return tenCon;
+    }
+
+    public void setTenCon(String tenCon) {
+        this.tenCon = tenCon;
+    }
+}
